7dfa7e441433b538664bbecaeacc3bb8378d0a3e
Alessandro Paperini <alessandro.paperini@drwolf.it>
2019-10-02 16:51:09 +0200

    :bulb: javadocs

--- src/main/java/it/laterale/cloud/config/RabbitMQConfig.java
+++ src/main/java/it/laterale/cloud/config/RabbitMQConfig.java
@@ -11,23 +11,44 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
+/**
+ * The type Rabbit mq config.
+ */
 @Configuration
 @Slf4j
 public class RabbitMQConfig {
 
     @Autowired
-    Environment env;
+    private Environment env;
 
+    /**
+     * Ball queue queue.
+     *
+     * @return the queue
+     */
     @Bean
     public Queue ballQueue() {
         return createQueue(this.env.getProperty("spring.rabbitmq.queue.ball.name"));
     }
 
+    /**
+     * Ball message adapter message listener adapter.
+     *
+     * @param consumer the consumer
+     * @return the message listener adapter
+     */
     @Bean
     public MessageListenerAdapter ballMessageAdapter(BallDataConsumer consumer) {
         return new MessageListenerAdapter(consumer, "receiveMessage");
     }
 
+    /**
+     * Ball message listener container simple message listener container.
+     *
+     * @param connectionFactory the connection factory
+     * @param consumer          the consumer
+     * @return the simple message listener container
+     */
     @Bean
     public SimpleMessageListenerContainer ballMessageListenerContainer(ConnectionFactory connectionFactory, BallDataConsumer consumer) {
         SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
--- src/main/java/it/laterale/cloud/consumers/BallDataConsumer.java
+++ src/main/java/it/laterale/cloud/consumers/BallDataConsumer.java
@@ -4,10 +4,18 @@ import it.laterale.cloud.dtos.Ball;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+/**
+ * The type Ball data consumer.
+ */
 @Component
 @Slf4j
 public class BallDataConsumer {
 
+    /**
+     * Receive message.
+     *
+     * @param data the data
+     */
     public void receiveMessage(Ball data) {
         // Do something
         log.info("Consuming ball data {}", data.toString());
